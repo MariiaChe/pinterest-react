d38672edccaad880ad5177110c44f9dbc7945a35
Mariia <maria.bookvoed@gmail.com>
2019-08-14 11:09:26 -0400

    navbar pinterest infinite scroll lib

--- pinterest/src/App.js
+++ pinterest/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+     images: []
+    }
+  }
+  componentDidMount(){
+   
+     this.fetchData();
+   
+  }
+  fetchData(){
+    fetch('https://pixabay.com/api/?key=13297531-ab9a2b78d5fb940f06962f0ef&image_type=photo&orientation=vertical')
+    .then(response=> response.json())
+    .then(images =>this.setState({images}))
+    .catch(error=>console.log('parsing failed', error))
+  }
+  
+  render(){
+    const { images } = this.state;
+    console.log(images);
+      return (
+        <React.Fragment>
+          <header>
+          <Navbar/>
+          </header>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="content">
+             
+          </div>
+        </React.Fragment>
+      );
+    }
 }
 
 export default App;
---
+++ pinterest/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Navbar.css';
+import Logo  from '../img/logo-pin.png';
+import Search from '../img/search.svg';
+import Bell from '../img/bell1.svg';
+import Chat from '../img/chat1.svg';
+import Dots from '../img/dot.svg';
+class Navbar extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="nav" id="navbar">
+                    <a href="">
+                    <div className="logo">
+                        <img className="img-fluid" src={Logo} alt="Logo"/>
+                    </div>
+                    </a>
+                    <div className="search">
+                        <div className="icon-search">
+                            <img src={Search} alt="search"/>
+                        </div>
+                        <input placeholder="Buscar" />
+                    </div>
+                    <div className="btn inicio">
+                       Inicio
+                    </div>
+                    <div className="btn siguiendo">
+                        Siguiendo
+                    </div>
+                    <div className="btn user">
+                        <div className="userPic">M</div>
+                        <div className="userName">María</div>
+                    </div>
+                    <div className="border"></div>
+                    <div className="message">
+                        <img src={Chat} alt="message"/>
+                    </div>
+                    <div className="news">
+                        <img src={Bell} alt="news"/>
+                    </div>
+                    <div className="add">
+                        <img src={Dots} alt="add"/>
+                    </div>
+                
+                <hr/>
+                </div>
+            </React.Fragment>
+        )
+    }
+   
+}
+export default Navbar

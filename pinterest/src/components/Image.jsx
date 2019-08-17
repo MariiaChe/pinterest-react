48930b58552ac631209756aab5f10456798fd639
Mariia <maria.bookvoed@gmail.com>
2019-08-17 01:13:00 -0400

    terminado comp  Image

--- pinterest/src/components/Image.jsx
+++ pinterest/src/components/Image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Image.css';
 import Pin from '../img/pin.png';
 import Dots from '../img/dot.svg';
-
+import Share from '../img/share.png'
 class Image extends React.Component{
     constructor(props) {
         super(props)
@@ -17,12 +17,13 @@ class Image extends React.Component{
 
     render(){
         var linkStyle;
-        var otrolinkstyle;
+        var othLinkStyle;
         if (this.state.hover) {
-            linkStyle = {display:'flex', position: 'absolute', top:'15px',right: '15px'};
-
+            linkStyle = {display:'flex', position: 'absolute', top:'15px',right: '15px'}
+            othLinkStyle={display:'block'}
           } else {
             linkStyle = {display:'none'}
+            othLinkStyle={display:'none'}
           }
         return(
             <React.Fragment>
@@ -30,7 +31,8 @@ class Image extends React.Component{
                     
                     <img className="img" src={this.props.url} alt=""/>
                     <button style={linkStyle}><img className="pin" src={Pin} />Guardar</button>
-                    <img className="dots" src={ Dots} />
+                    <div className="circleDots"><img className="dots" src={ Dots} /></div>
+                    <img style={othLinkStyle} className="share" src={ Share } />
                 </div>
             </React.Fragment>
         )

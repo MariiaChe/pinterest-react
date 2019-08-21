10aac1d56d28ba7d68426876d3a5f57b252f22bd
Mariia <maria.bookvoed@gmail.com>
2019-08-21 18:20:18 -0400

    concat en fetchData para que funcione infinite scroll

--- pinterest/src/App.js
+++ pinterest/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Image from './components/Image';
 import Modal from './components/Modal';
+import InfiniteScroll from 'react-infinite-scroll-component'
 //import MasonryInfiniteScroller from 'react-masonry-infinite';
 // import { Grid, GridItem } from 'react-masonry-infinite-scroll';
 import Masonry from 'react-masonry-css';
@@ -11,17 +12,23 @@ class App extends React.Component {
     super(props);
     this.state={
      images: [],
-     query:''
-    }
-  
+     query:'',
+     count: 1
     
+    }
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleChange= this.handleChange.bind(this);
     this.searchImg = this.searchImg.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+   
 }
 
 componentDidMount(){
-  this.searchImg(this.state.query)
+
+  fetch(`https://pixabay.com/api/?key=13297531-ab9a2b78d5fb940f06962f0ef&image_type=photo&orientation=vertical&per_page=20`)
+  .then(response=> response.json())
+  .then(data =>this.setState({images: data.hits}))
+  .catch(error=>console.log('parsing failed', error))
 }
 
 handleChange (e) {    
@@ -36,42 +43,67 @@ handleKeyPress(e) {
   }
 }
 searchImg(query) {
-  fetch(`https://pixabay.com/api/?key=13297531-ab9a2b78d5fb940f06962f0ef&q=${query}&image_type=photo&orientation=vertical&per_page=200`)
+  fetch(`https://pixabay.com/api/?key=13297531-ab9a2b78d5fb940f06962f0ef&q=${query}&image_type=photo&orientation=vertical&per_page=20`)
   .then(response=> response.json())
   .then(data =>this.setState({images: data.hits}))
   .catch(error=>console.log('parsing failed', error))
 }
+fetchData(){
+  const { count } = this.state;
+  this.setState({ count: this.state.count + 1});
+  fetch(`https://pixabay.com/api/?key=13297531-ab9a2b78d5fb940f06962f0ef&image_type=photo&orientation=vertical&page=${count}&per_page=20`)
+  .then(response=> response.json())
+  .then(data =>this.setState({images: this.state.images.concat(data.hits)}))
+  .catch(error=>console.log('parsing failed', error))
+  
+}
+showImg(){
+  return(
+    this.state.images.map((image)=>{
+      return(
+        <Image dataTarget={image.id} key={image.id} url={image.webformatURL} />
+      )
+    })
+  )
+}
+showModal(){
+  return(
+    this.state.images.map((image)=>{
+      return(
+        <Modal key={image.id} id={image.id} url={image.webformatURL} />
+      )
+    })
+  )
+}
 render(){
+  
     const breakpointColumnsObj = {
       default: 4,
       1100: 3,
       700: 2,
       500: 1
     };
-    const { images } = this.state;
-    console.log(images);
+   
       return (
 
         <React.Fragment>
           <header>
             <Navbar  handleChange={this.handleChange} handleKeyPress={this.handleKeyPress} />
           </header>
-          {images.map(image=>
-          <Modal id={image.id} url={image.webformatURL} />)}
+            {this.showModal()}
           <div className="content container">
-            
-          <Masonry
-            breakpointCols={breakpointColumnsObj}
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column">
-
-             {images.map(image=>
-             <Image dataTarget={image.id} key={image.id} url={image.webformatURL} />
-         
-             
-              )}
-             
+          <InfiniteScroll
+          dataLength={this.state.images.length}
+          next={this.fetchData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}>
+            <Masonry
+              breakpointCols={breakpointColumnsObj}
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column">
+              {this.showImg()}
             </Masonry>
+          </InfiniteScroll>
             
           </div>
         </React.Fragment>

4798bf964b754ae6c6c01f2547694eed8a075579
Mariia <maria.bookvoed@gmail.com>
2019-08-20 22:41:17 -0400

    Updates

---
+++ precache-manifest.e02f89a225ffbabd3170ab1780a4c9e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1862efd03fbedaf69f72ea3352486621",
+    "url": "/pinterest-react/index.html"
+  },
+  {
+    "revision": "11556c9b44e49a93414f",
+    "url": "/pinterest-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "71230a44d932aef9afa5",
+    "url": "/pinterest-react/static/css/main.bffce989.chunk.css"
+  },
+  {
+    "revision": "11556c9b44e49a93414f",
+    "url": "/pinterest-react/static/js/2.a719959a.chunk.js"
+  },
+  {
+    "revision": "71230a44d932aef9afa5",
+    "url": "/pinterest-react/static/js/main.a952e33b.chunk.js"
+  },
+  {
+    "revision": "c3071c6547994c276ef6",
+    "url": "/pinterest-react/static/js/runtime~main.cdad5a52.js"
+  },
+  {
+    "revision": "7685b8f781afba276e6902f2704727fe",
+    "url": "/pinterest-react/static/media/arrowback.7685b8f7.svg"
+  },
+  {
+    "revision": "8e7b8aee0944d04208c156a2621ba27f",
+    "url": "/pinterest-react/static/media/bell1.8e7b8aee.svg"
+  },
+  {
+    "revision": "6d13d7e7b54a3ee5b3ed23e691255e7c",
+    "url": "/pinterest-react/static/media/cancel.6d13d7e7.svg"
+  },
+  {
+    "revision": "fb68c5b1c2776d92a036333a4f97371c",
+    "url": "/pinterest-react/static/media/chat1.fb68c5b1.svg"
+  },
+  {
+    "revision": "1650d6e89990e5337b2726b42efec4d5",
+    "url": "/pinterest-react/static/media/dot.1650d6e8.svg"
+  },
+  {
+    "revision": "6de1742d065d8e3a8a7b545b9014cef4",
+    "url": "/pinterest-react/static/media/logo-pin.6de1742d.png"
+  },
+  {
+    "revision": "a8318a4390c35368096553db0b98865d",
+    "url": "/pinterest-react/static/media/search.a8318a43.svg"
+  }
+]);
